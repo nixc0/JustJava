a7a282acc684fbd21c8b0d49b89e14acc9d0c4d5
curtis <nixc@g.cofc.edu>
2018-02-21 18:04:07 -0500

    Fixed the order button. Set the price to $0 initially.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,19 +22,21 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 5;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5 );
+        displayPrice(quantity * 5 );
     }
 
     /**
@@ -56,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the increment button is clicked.
      */
     public void increment(View view){
-        int quantity = 2;
         quantity = quantity + 1;
         display(quantity);
     }
@@ -65,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the decrement button is clicked.
      */
     public void decrement(View view){
-        int quantity = 2;
         quantity = quantity - 1;
         display(quantity);
     }
